76a010117ffebe099fb13894cf2dfbf97ec07a98
Aryan Gupta <aryanbagicha@gmail.com>
2026-03-12 23:03:25 +0530

    Beautiful Year

---
+++ Difficulty: 800/BeautifulYear.java
@@ -0,0 +1,56 @@
+//Problem
+
+    /*It seems like the year of 2013 came only yesterday. Do you know a curious fact? The year of 2013 is the first year after the old 1987 with only distinct digits.
+    
+    Now you are suggested to solve the following problem: given a year number, find the minimum year number which is strictly larger than the given one and has only distinct digits.
+    
+    Input
+    The single line contains integer y (1000 ≤ y ≤ 9000) — the year number.
+    
+    Output
+    Print a single integer — the minimum year number that is strictly larger than y and all it's digits are distinct. It is guaranteed that the answer exists.
+    
+    Examples
+    InputCopy
+    1987
+    OutputCopy
+    2013
+    InputCopy
+    2013
+    OutputCopy
+    2014
+    */
+
+//Solution
+
+import java.util.*;
+ 
+public class Main {
+    public static boolean distinct(int year){
+        String s = String.valueOf(year);
+        HashSet<Character> set = new HashSet<>();
+ 
+        for(char c : s.toCharArray()){
+            if(set.contains(c)){
+                return false;
+            }
+            set.add(c);
+        }
+        return true;
+    }
+ 
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int y = sc.nextInt();
+ 
+        int year = y + 1;
+ 
+        while(true){
+            if(distinct(year)){
+                System.out.println(year);
+                break;
+            }
+            year++;
+        }
+    }
+}
