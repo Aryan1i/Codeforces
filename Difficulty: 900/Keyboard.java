8c7376ae96bc79f0ee48548e5788d74b3493533a
Aryan Gupta <aryanbagicha@gmail.com>
2026-04-26 21:45:21 +0530

    Keyboard

---
+++ Difficulty: 900/Keyboard.java
@@ -0,0 +1,61 @@
+    //Problem
+    
+    /*Keyboard
+    time limit per test2 seconds
+    memory limit per test256 megabytes
+    Our good friend Mole is trying to code a big message. He is typing on an unusual keyboard with characters arranged in following way:
+    
+    
+    qwertyuiop
+    asdfghjkl;
+    zxcvbnm,./
+    Unfortunately Mole is blind, so sometimes it is problem for him to put his hands accurately. He accidentally moved both his hands with one position to the left or to the right. That means that now he presses not a button he wants, but one neighboring button (left or right, as specified in input).
+    
+    We have a sequence of characters he has typed and we want to find the original message.
+    
+    Input
+    First line of the input contains one letter describing direction of shifting ('L' or 'R' respectively for left or right).
+    
+    Second line contains a sequence of characters written by Mole. The size of this sequence will be no more than 100. Sequence contains only symbols that appear on Mole's keyboard. It doesn't contain spaces as there is no space on Mole's keyboard.
+    
+    It is guaranteed that even though Mole hands are moved, he is still pressing buttons on keyboard and not hitting outside it.
+    
+    Output
+    Print a line that contains the original message.
+    
+    Examples
+    InputCopy
+    R
+    s;;upimrrfod;pbr
+    OutputCopy
+    allyouneedislove
+    */
+
+//Solution
+
+import java.util.*;
+ 
+public class Keyboard {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+ 
+        char direction = sc.next().charAt(0);
+        String typed = sc.next();
+ 
+        String keyboard = "qwertyuiopasdfghjkl;zxcvbnm,./";
+ 
+        StringBuilder result = new StringBuilder();
+ 
+        for (char ch : typed.toCharArray()) {
+            int index = keyboard.indexOf(ch);
+ 
+            if (direction == 'R') {
+                result.append(keyboard.charAt(index - 1));
+            } else {
+                result.append(keyboard.charAt(index + 1));
+            }
+        }
+ 
+        System.out.println(result.toString());
+    }
+}
